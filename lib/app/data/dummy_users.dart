03e96d1ac21e00324d4b8494abd3cd2bab15a97c
rama <mramayusuf3@gmail.com>
2026-05-18 18:59:58 +0700

    feat: membuat data dummy

---
+++ lib/app/data/dummy_users.dart
@@ -0,0 +1,17 @@
+class DummyUsers {
+  static final List<Map<String, dynamic>> users = [
+    {
+      'email': 'petugas@pdam.com',
+      'password': '123456',
+      'role': 'petugas',
+      'nama': 'Budi Petugas',
+    },
+    {
+      'email': 'pelanggan@pdam.com',
+      'password': '123456',
+      'role': 'pelanggan',
+      'nama': 'Andi Pelanggan',
+      'nomorMeter': 'PDAM-001289',
+    },
+  ];
+}
