5aeafec518e5aa86384c14ae33d9c43e6df50bb2
rama <mramayusuf3@gmail.com>
2026-05-18 18:56:06 +0700

    feat: setup konfigurasi routes

---
+++ lib/app/routes/app_pages.dart
@@ -0,0 +1,43 @@
+import 'package:get/get.dart';
+
+import '../modules/beranda_petugas/bindings/beranda_petugas_binding.dart';
+import '../modules/beranda_petugas/views/beranda_petugas_view.dart';
+import '../modules/login/bindings/login_binding.dart';
+import '../modules/login/views/login_view.dart';
+import '../modules/onboarding/bindings/onboarding_binding.dart';
+import '../modules/onboarding/views/onboarding_view.dart';
+
+part 'app_routes.dart';
+
+abstract class AppPages {
+  static const INITIAL = Routes.ONBOARDING;
+
+  static final routes = [
+    GetPage(
+      name: Routes.ONBOARDING,
+      page: () => const OnboardingView(),
+      binding: OnboardingBinding(),
+      transition: Transition.fadeIn,
+      transitionDuration: const Duration(milliseconds: 300),
+    ),
+
+    GetPage(
+      name: Routes.LOGIN,
+      page: () => const LoginView(),
+      binding: LoginBinding(),
+      transition: Transition.fadeIn,
+      transitionDuration: const Duration(milliseconds: 300),
+    ),
+
+    // PETUGAS
+    GetPage(
+      name: Routes.BERANDA_PETUGAS,
+      page: () => const BerandaPetugasView(),
+      binding: BerandaPetugasBinding(),
+      transition: Transition.fadeIn,
+      transitionDuration: const Duration(milliseconds: 300),
+    ),
+
+
+  ];
+}
---
+++ lib/app/routes/app_routes.dart
@@ -0,0 +1,18 @@
+part of 'app_pages.dart';
+// DO NOT EDIT. This is code generated via package:get_cli/get_cli.dart
+
+abstract class Routes {
+  static const ONBOARDING = _Paths.ONBOARDING;
+  static const LOGIN = _Paths.LOGIN;
+
+  static const BERANDA_PETUGAS = _Paths.BERANDA_PETUGAS;
+  static const BERANDA_PELANGGAN = _Paths.BERANDA_PELANGGAN;
+}
+
+abstract class _Paths {
+  static const ONBOARDING = '/';
+  static const LOGIN = '/login';
+
+  static const BERANDA_PETUGAS = '/beranda-petugas';
+  static const BERANDA_PELANGGAN = '/beranda-pelanggan';
+}
